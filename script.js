71a788dcf9396c10d0f8222c98aa5e8fe91b123e
zukki4123 <zukki4123g@gmail.com>
2026-04-25 03:23:51 +0900

    first

---
+++ script.js
@@ -0,0 +1,11 @@
+const fades = document.querySelectorAll(".fade");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
+  });
+}, { threshold: 0.15 });
+
+fades.forEach(el => observer.observe(el));
